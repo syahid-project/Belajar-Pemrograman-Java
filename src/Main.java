a2b966c4569a3c73eb7c159d07854c9cac1d4375
syahid-project <saidsetiawan774@gmail.com>
2025-10-23 21:20:14 +0700

    Update main class with version info

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello, BelajarProgramJava!");
+        System.out.println("Version 1.1 - CodeLab 1");
     }
 }
