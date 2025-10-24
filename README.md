0718f43bee580dd11b693cd9900ceac9cf0a2ca1
syahid-project <saidsetiawan774@gmail.com>
2025-10-24 16:29:57 +0700

    Update README.md

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
 # Deskripsi Program
 Program sederhana untuk Demo **CodeLab 2**.  
-Tujuan program ini untuk menampilkan teks di konsol.
+Tujuan program ini untuk membuat dokumentasi hasil program yang kita buat dengan program java yang menggunakan perulangan **for** dan percabangan **if**.
 
-Disini saya menggunakan class yang bernama **Main**
+Disini saya menggunakan class yang bernama **Main**. disini program meminta pengguna memasukkan **Nama** dan **angka favorit** (1-5). Lalu program akan menampilkan daftar angka dari 1 sampai 5 menggunakan perulangan **for** dan memberikan **pesan khusus** menggunakan **percabangan if** berdasarkan angka yang dimasukkan.
 
 Gunakan perintah `System.out.println()` untuk menampilkan teks.
 - program menampilkan output:
 ```
-Hello, Disini kita akan membuat program!
-CodeLab 1 dan 2
+Masukkan nama kamu: syahid
+Masukkan angka favorit kamu (1-5): 2
+
+Hai syahid! Ini daftar angka dari 1 sampai 5:
+Angka ke-1
+Angka ke-2
+Angka ke-3
+Angka ke-4
+Angka ke-5
+Angka 2 juga keren kok, tetap semangat belajar Java!
+
+Program selesai. Terima kasih sudah mencoba!
 ```
 
 ## Pembuat
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello, Disini kita akan membuat program!");
-        System.out.println("CodeLab 1 dan 2");
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("=== Program Latihan CodeLab 1 dan 2 ===");
+        System.out.print("Masukkan nama kamu: ");
+        String nama = input.nextLine();
+
+        System.out.print("Masukkan angka favorit kamu (1-5): ");
+        int angka = input.nextInt();
+
+        System.out.println("\nHai " + nama + "! Ini daftar angka dari 1 sampai 5:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Angka ke-" + i);
+        }
+
+        if (angka == 1) {
+            System.out.println("Kamu suka angka 1! Itu melambangkan awal yang baru.");
+        } else if (angka == 5) {
+            System.out.println("Angka 5 adalah simbol keberuntungan! Pilihan yang bagus!");
+        } else {
+            System.out.println("Angka " + angka + " juga keren kok, tetap semangat belajar Java!");
+        }
+
+        System.out.println("\nProgram selesai. Terima kasih sudah mencoba!");
+        input.close();
     }
 }
